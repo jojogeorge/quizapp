0a48bda46f8238898b1430acb1e802ad64355e77
George <jojgeorge@deloitte.com>
2020-06-25 20:19:04 +0530

    Updates

---
+++ precache-manifest.211cf4a3c60e88608f7274f7e62f44d4.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4216a4e73ff36e16ab15fd087bec68d3",
+    "url": "/quizapp/index.html"
+  },
+  {
+    "revision": "a38f0e56e50219499e4b",
+    "url": "/quizapp/static/css/2.36020323.chunk.css"
+  },
+  {
+    "revision": "2a61cb19418bc8f984fa",
+    "url": "/quizapp/static/css/main.52cf0d9b.chunk.css"
+  },
+  {
+    "revision": "a38f0e56e50219499e4b",
+    "url": "/quizapp/static/js/2.42eed0dc.chunk.js"
+  },
+  {
+    "revision": "35937c55f4ccbb69aaefc08b51c916a0",
+    "url": "/quizapp/static/js/2.42eed0dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a61cb19418bc8f984fa",
+    "url": "/quizapp/static/js/main.391d4100.chunk.js"
+  },
+  {
+    "revision": "54793ab1b2fa4cce1ad3",
+    "url": "/quizapp/static/js/runtime-main.545a8c06.js"
+  },
+  {
+    "revision": "f356b66151c928021cabf492fdaa7803",
+    "url": "/quizapp/static/media/background.f356b661.jpg"
+  },
+  {
+    "revision": "2ca6ffd1fc895bd2549ac0f94b0bf41d",
+    "url": "/quizapp/static/media/button-click.2ca6ffd1.ogg"
+  },
+  {
+    "revision": "8d6df0fbf853139d683f3165140c3257",
+    "url": "/quizapp/static/media/error.8d6df0fb.wav"
+  },
+  {
+    "revision": "94c9db16c11944895d6daafc80521f1c",
+    "url": "/quizapp/static/media/hints.94c9db16.png"
+  },
+  {
+    "revision": "a70319b5b42be465bc0979fde9c6b011",
+    "url": "/quizapp/static/media/success.a70319b5.wav"
+  }
+]);
